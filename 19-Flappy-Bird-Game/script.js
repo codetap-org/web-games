f2669eeac1799539e6cab6bbe944533408bfc48b
Monish Roy <125638057+monishroy@users.noreply.github.com>
2025-05-27 08:32:56 +0600

    tetris and flappy bird game updae

--- 19-Flappy-Bird-Game/script.js
+++ 19-Flappy-Bird-Game/script.js
@@ -1,207 +1,108 @@
-var ctx = myCanvas.getContext("2d");
-var FPS = 30;
-var jump_amount = -10;
-var max_fall_speed = +10;
-var acceleration = 1;
-var pipe_speed = -2;
-var game_mode = "prestart";
-var time_game_last_running;
-var bottom_bar_offset = 0;
-var pipes = [];
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const img = new Image();
+img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
 
-function MySprite(img_url) {
-  this.x = 0;
-  this.y = 0;
-  this.visible = true;
-  this.velocity_x = 0;
-  this.velocity_y = 0;
-  this.MyImg = new Image();
-  this.MyImg.src = img_url || "";
-  this.angle = 0;
-  this.flipV = false;
-  this.flipH = false;
+// general settings
+let gamePlaying = false;
+const gravity = .5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0,
+  bestScore = 0,
+  flight,
+  flyHeight,
+  currentScore,
+  pipe;
+
+// pipe settings
+const pipeWidth = 78;
+const pipeGap = 270;
+const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGap + pipeWidth)) - pipeWidth)) + pipeWidth;
+
+const setup = () => {
+  currentScore = 0;
+  flight = jump;
+
+  // set initial flyHeight (middle of screen - size of the bird)
+  flyHeight = (canvas.height / 2) - (size[1] / 2);
+
+  // setup first 3 pipes
+  pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
 }
-MySprite.prototype.Do_Frame_Things = function () {
-  ctx.save();
-  ctx.translate(this.x + this.MyImg.width / 2, this.y + this.MyImg.height / 2);
-  ctx.rotate((this.angle * Math.PI) / 180);
-  if (this.flipV) ctx.scale(1, -1);
-  if (this.flipH) ctx.scale(-1, 1);
-  if (this.visible)
-    ctx.drawImage(this.MyImg, -this.MyImg.width / 2, -this.MyImg.height / 2);
-  this.x = this.x + this.velocity_x;
-  this.y = this.y + this.velocity_y;
-  ctx.restore();
-};
-function ImagesTouching(thing1, thing2) {
-  if (!thing1.visible || !thing2.visible) return false;
-  if (
-    thing1.x >= thing2.x + thing2.MyImg.width ||
-    thing1.x + thing1.MyImg.width <= thing2.x
-  )
-    return false;
-  if (
-    thing1.y >= thing2.y + thing2.MyImg.height ||
-    thing1.y + thing1.MyImg.height <= thing2.y
-  )
-    return false;
-  return true;
-}
-function Got_Player_Input(MyEvent) {
-  switch (game_mode) {
-    case "prestart": {
-      game_mode = "running";
-      break;
-    }
-    case "running": {
-      bird.velocity_y = jump_amount;
-      break;
-    }
-    case "over":
-      if (new Date() - time_game_last_running > 1000) {
-        reset_game();
-        game_mode = "running";
-        break;
+
+const render = () => {
+  // make the pipe and bird moving 
+  index++;
+
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // background first part 
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
+  // background second part
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
+
+  // pipe display
+  if (gamePlaying) {
+    pipes.map(pipe => {
+      // pipe moving
+      pipe[0] -= speed;
+
+      // top pipe
+      ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+      // bottom pipe
+      ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+
+      // give 1 point & create new pipe
+      if (pipe[0] <= -pipeWidth) {
+        currentScore++;
+        // check if it's the best score
+        bestScore = Math.max(bestScore, currentScore);
+
+        // remove & create new pipe
+        pipes = [...pipes.slice(1), [pipes[pipes.length - 1][0] + pipeGap + pipeWidth, pipeLoc()]];
+        console.log(pipes);
       }
+
+      // if hit the pipe, end
+      if ([
+        pipe[0] <= cTenth + size[0],
+        pipe[0] + pipeWidth >= cTenth,
+        pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]
+      ].every(elem => elem)) {
+        gamePlaying = false;
+        setup();
+      }
+    })
   }
-  MyEvent.preventDefault();
-}
-addEventListener("touchstart", Got_Player_Input);
-addEventListener("mousedown", Got_Player_Input);
-addEventListener("keydown", Got_Player_Input);
-function make_bird_slow_and_fall() {
-  if (bird.velocity_y < max_fall_speed) {
-    bird.velocity_y = bird.velocity_y + acceleration;
+  // draw bird
+  if (gamePlaying) {
+    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+    flight += gravity;
+    flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+  } else {
+    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size[0] / 2), flyHeight, ...size);
+    flyHeight = (canvas.height / 2) - (size[1] / 2);
+    // text accueil
+    ctx.fillText(`Best score : ${bestScore}`, 85, 245);
+    ctx.fillText('Click to play', 90, 535);
+    ctx.font = "bold 30px courier";
   }
-  if (bird.y > myCanvas.height - bird.MyImg.height) {
-    bird.velocity_y = 0;
-    game_mode = "over";
-  }
-  if (bird.y < 0 - bird.MyImg.height) {
-    bird.velocity_y = 0;
-    game_mode = "over";
-  }
+
+  document.getElementById('bestScore').innerHTML = `Best : ${bestScore}`;
+  document.getElementById('currentScore').innerHTML = `Current : ${currentScore}`;
+
+  // tell the browser to perform anim
+  window.requestAnimationFrame(render);
 }
 
-function add_pipe(x_pos, top_of_gap, gap_width) {
-  var top_pipe = new MySprite();
-  top_pipe.MyImg = pipe_piece;
-  top_pipe.x = x_pos;
-  top_pipe.y = top_of_gap - pipe_piece.height;
-  top_pipe.velocity_x = pipe_speed;
-  pipes.push(top_pipe);
-  var bottom_pipe = new MySprite();
-  bottom_pipe.MyImg = pipe_piece;
-  bottom_pipe.flipV = true;
-  bottom_pipe.x = x_pos;
-  bottom_pipe.y = top_of_gap + gap_width;
-  bottom_pipe.velocity_x = pipe_speed;
-  pipes.push(bottom_pipe);
-}
-function make_bird_tilt_appropriately() {
-  if (bird.velocity_y < 0) {
-    bird.angle = -15;
-  } else if (bird.angle < 70) {
-    bird.angle = bird.angle + 4;
-  }
-}
-function show_the_pipes() {
-  for (var i = 0; i < pipes.length; i++) {
-    pipes[i].Do_Frame_Things();
-  }
-}
-function check_for_end_game() {
-  for (var i = 0; i < pipes.length; i++)
-    if (ImagesTouching(bird, pipes[i])) game_mode = "over";
-}
-function display_intro_instructions() {
-  ctx.font = "25px Arial";
-  ctx.fillStyle = "red";
-  ctx.textAlign = "center";
-  ctx.fillText(
-    "Press, touch or click to start",
-    myCanvas.width / 2,
-    myCanvas.height / 4
-  );
-}
-function display_game_over() {
-  var score = 0;
-  for (var i = 0; i < pipes.length; i++)
-    if (pipes[i].x < bird.x) score = score + 0.5;
-  ctx.font = "30px Arial";
-  ctx.fillStyle = "red";
-  ctx.textAlign = "center";
-  ctx.fillText("Game Over", myCanvas.width / 2, 100);
-  ctx.fillText("Score: " + score, myCanvas.width / 2, 150);
-  ctx.font = "20px Arial";
-  ctx.fillText("Click, touch, or press to play again", myCanvas.width / 2, 300);
-}
-function display_bar_running_along_bottom() {
-  if (bottom_bar_offset < -23) bottom_bar_offset = 0;
-  ctx.drawImage(
-    bottom_bar,
-    bottom_bar_offset,
-    myCanvas.height - bottom_bar.height
-  );
-}
-function reset_game() {
-  bird.y = myCanvas.height / 2;
-  bird.angle = 0;
-  pipes = []; // erase all the pipes from the array
-  add_all_my_pipes(); // and load them back in their starting positions
-}
-function add_all_my_pipes() {
-  add_pipe(500, 100, 140);
-  add_pipe(800, 50, 140);
-  add_pipe(1000, 250, 140);
-  add_pipe(1200, 150, 120);
-  add_pipe(1600, 100, 120);
-  add_pipe(1800, 150, 120);
-  add_pipe(2000, 200, 120);
-  add_pipe(2200, 250, 120);
-  add_pipe(2400, 30, 100);
-  add_pipe(2700, 300, 100);
-  add_pipe(3000, 100, 80);
-  add_pipe(3300, 250, 80);
-  add_pipe(3600, 50, 60);
-  var finish_line = new MySprite("http://s2js.com/img/etc/flappyend.png");
-  finish_line.x = 3900;
-  finish_line.velocity_x = pipe_speed;
-  pipes.push(finish_line);
-}
-var pipe_piece = new Image();
-pipe_piece.onload = add_all_my_pipes;
-pipe_piece.src = "http://s2js.com/img/etc/flappypipe.png";
-function Do_a_Frame() {
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  bird.Do_Frame_Things();
-  display_bar_running_along_bottom();
-  switch (game_mode) {
-    case "prestart": {
-      display_intro_instructions();
-      break;
-    }
-    case "running": {
-      time_game_last_running = new Date();
-      bottom_bar_offset = bottom_bar_offset + pipe_speed;
-      show_the_pipes();
-      make_bird_tilt_appropriately();
-      make_bird_slow_and_fall();
-      check_for_end_game();
-      break;
-    }
-    case "over": {
-      make_bird_slow_and_fall();
-      display_game_over();
-      break;
-    }
-  }
-}
-var bottom_bar = new Image();
-bottom_bar.src = "http://s2js.com/img/etc/flappybottom.png";
+// launch setup
+setup();
+img.onload = render;
 
-var bird = new MySprite("http://s2js.com/img/etc/flappybird.png");
-bird.x = myCanvas.width / 3;
-bird.y = myCanvas.height / 2;
-
-setInterval(Do_a_Frame, 1000 / FPS);
+// start game
+document.addEventListener('click', () => gamePlaying = true);
+window.onclick = () => flight = jump;
